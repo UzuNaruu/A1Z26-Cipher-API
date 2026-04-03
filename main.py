bcc962d92a9ac91e2e6a601248e2838f9e77bf31
Ulaş <ulasgksgl@gmail.com>
2026-04-03 22:38:53 +0300

    Bug Fix Live Server 2

--- main.py
+++ main.py
@@ -8,6 +8,16 @@ app = FastAPI()
 def get_db_cursor():
     connect = sqlite3.connect("password_saves.db")
     cursor = connect.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS Passwords(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        original_message TEXT,
+        type TEXT,
+        result TEXT
+    )
+    """)
+    connect.commit()
     return connect, cursor
 
 
